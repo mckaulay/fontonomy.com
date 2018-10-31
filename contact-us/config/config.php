9a2a1247d124b8e2d1deaaec1c35e5ee1ddd2a6b
mckaulay <mckaulay+github@gmail.com>
2018-10-30 18:37:09 -0700

    add existing code

---
+++ contact-us/config/config.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'subject' => [
+        'prefix' => '[Contact Form]'
+    ],
+    'emails' => [
+        'to'   => 'mckaulay@gmail.com',
+        'from' => 'contact@fontonomy.com'
+    ],
+    'messages' => [
+        'error'   => 'There was an error sending, please try again later.',
+        'success' => 'Your message has been sent successfully.'
+    ],
+    'fields' => [
+        'name'     => 'Name',
+        'email'    => 'Email',
+        'subject'  => 'Subject',
+        'message'  => 'Message',
+        'btn-send' => 'Send'
+    ]
+];
